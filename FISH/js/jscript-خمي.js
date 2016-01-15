695b0fe1a6ed953b5f966ce37b12e70ae84554f2
sara magdy <sara magdy>
2016-01-15 13:18:38 -0800

    level1,2 complete zina sara ghada bassem

--- FISH/js/jscript-خمي.js
+++ FISH/js/jscript-خمي.js
@@ -15,13 +15,6 @@
  var game;
  var header;
  var sound;
- //////////////////////////////
-var smallFish=[];
-var midFish=[];
-var bigFish=[];
-var LeftDiamon=false;
-var MiddleDiamon=false;
-var RightDiamon=false;
 //////////////////////////---start menu---/////////////////////////
 function displayMenu(){             
 //---------getElements------------------------------------------------	   
@@ -211,7 +204,7 @@ function mouseCurser(){ //myFishSize 0--small, 1---mid, 2----big
 ////////-------creat fish----------------------//
 	function creatSmallFish()
 	{
-		var t=get_random(50,570);
+		var t=get_random(200,700);
 
 		var img=document.createElement("img");
 		img.setAttribute("src","images/small_right.png" );
@@ -219,14 +212,14 @@ function mouseCurser(){ //myFishSize 0--small, 1---mid, 2----big
 		img.setAttribute("id","small1");
 		img.setAttribute("class","smallFish");
 
-		t=get_random(50,570);
+		t=get_random(200,700);
 		var img1=document.createElement("img");
 		img1.setAttribute("src","images/small_right.png" );
 		img1.setAttribute("style","position:absolute ;TOP:"+t+"px; LEFT:200px  ; width:50px ; hieght:50px");
 		img1.setAttribute("id","small2");
 		img1.setAttribute("class","smallFish");
-		
-		t=get_random(50,570);
+
+		t=get_random(200,700);
 		var img2=document.createElement("img");
 		img2.setAttribute("src","images/small_left.png" );
 		img2.setAttribute("id","small3" );
@@ -256,27 +249,26 @@ function mouseCurser(){ //myFishSize 0--small, 1---mid, 2----big
 	function creatMidFish()
 	{
 
-		var t=get_random(50,570);
+		var t=get_random(200,800);
 		var img=document.createElement("img");
 		img.setAttribute("src","images/mid_right.png" );
 		img.setAttribute("style","position:absolute ;TOP:"+t+"px; LEFT:200px ; width:60px ; hieght:60px");
 		img.setAttribute("id","mid1");
 		img.setAttribute("class","midFish");
 
-		t=get_random(50,570);
+		t=get_random(200,800);
 		var img1=document.createElement("img");
 		img1.setAttribute("src","images/mid_left.png" );
 		img1.setAttribute("style","position:absolute ;TOP:"+t+"px; LEFT:1050px  ; width:60px ; hieght:60px");
 		img1.setAttribute("id","mid2");
 		img1.setAttribute("class","midFish");
 
-		t=get_random(50,570);
+		t=get_random(200,800);
 		var img2=document.createElement("img");
 		img2.setAttribute("src","images/mid_left.png" );
 		img2.setAttribute("style","position:absolute ;TOP:"+t+"px; LEFT:1050px  ; width:60px ; hieght:60px");
 		img2.setAttribute("id","mid3");
 		img2.setAttribute("class","midFish");
-
 
 		game.appendChild(img);
 		game.appendChild(img1);
@@ -323,11 +315,10 @@ function mouseCurser(){ //myFishSize 0--small, 1---mid, 2----big
 		img1.setAttribute("style","position:absolute ;TOP:500px; LEFT:1050px; width:80px ; hieght:80px");
 		img1.setAttribute("id","big2");
 		img1.setAttribute("class","bigFish");
-
-
 		
 		game.appendChild(img);
 		game.appendChild(img1);
+		///-------detect-------//
 		img.addEventListener('mouseover',bigfish);
 		img1.addEventListener('mouseover',bigfish);
 		function bigfish(){
@@ -342,110 +333,6 @@ function mouseCurser(){ //myFishSize 0--small, 1---mid, 2----big
 			   }
 		}
 	}
-	////////////////////////
-	function drowDiamond()
-	{
-		//////////////////// Diamond left \\\\\\\\\\\\\\\\\\\\\\\\\\
-		var img=document.createElement("img");
-		img.setAttribute("src","images/close.png" );
-		img.setAttribute("style","position:absolute ;TOP:550px; LEFT:250px ; width:60px ; hieght:60px");
-		img.setAttribute("id","left");
-		game.appendChild(img);open
-
-		//////////////////// Diamond in middle \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-		var img=document.createElement("img");
-		img.setAttribute("src","images/close.png" );
-		img.setAttribute("style","position:absolute ;TOP:550px; LEFT:625px ; width:60px ; hieght:60px");
-		img.setAttribute("id","middle");;
-		game.appendChild(img);
-
-		///////////////////// Diamond right \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-		var img=document.createElement("img");
-		img.setAttribute("src","images/close.png" );
-		img.setAttribute("style","position:absolute ;TOP:550px; LEFT:1000px ; width:60px ; hieght:60px");
-		img.setAttribute("id","right");
-
-		game.appendChild(img);
-
-
-	}
-	///////////////////////////////////
-	function openLeft()
-	{
-		var leftDiamon=document.getElementById("left");
-		leftDiamon.src="images/open.png";
-
-		if (LeftDiamon) {
-			var imgD=document.createElement("img");
-			imgD.setAttribute("src","images/diamond.png" );
-			imgD.setAttribute("style","position:absolute ;TOP:555px; LEFT:257px ");
-			imgD.setAttribute("id","leftD");
-			game.appendChild(imgD);
-		}
-	}
-
-	function openInMiddle()
-	{
-		var middleDiamon=document.getElementById("middle");
-		middleDiamon.src="images/open.png";
-
-		if (MiddleDiamon) {
-			var imgD=document.createElement("img");
-			imgD.setAttribute("src","images/diamond.png" );
-			imgD.setAttribute("style","position:absolute ;TOP:555px; LEFT:632px ");
-			imgD.setAttribute("id","middleD");
-			game.appendChild(imgD);
-		}
-	}
-
-	function openRight()
-	{
-		var rightDiamon=document.getElementById("right");
-		rightDiamon.src="images/open.png";
-
-		if (RightDiamon) {
-			var imgD=document.createElement("img");
-			imgD.setAttribute("src","images/diamond.png" );
-			imgD.setAttribute("style","position:absolute ;TOP:555px; LEFT:1007px ");
-			imgD.setAttribute("id","rightD");
-			game.appendChild(imgD);
-			
-		}
-	}
-
-	function closeLeft(){
-
-		var left=document.getElementById("leftD");
-		game.removeChild(left);
-
-		var leftDiamon=document.getElementById("left");
-		leftDiamon.src="images/close.png";
-
-		
-	}
-
-	function closemiddle(){
-
-		var middle=document.getElementById("middleD");
-		game.removeChild(middle);
-
-		var middleDiamon=document.getElementById("middle");
-		middleDiamon.src="images/close.png";
-
-		
-	}
-
-	function closeRight(){
-
-		var right=document.getElementById("rightD");
-		game.removeChild(right);
-
-		var rightDiamon=document.getElementById("right");
-		rightDiamon.src="images/close.png";
-
-		
-	}
-////////////////////////////////////////////////////////////////
 /////////------move--------------//////
 
 	function moveSmall()
@@ -462,18 +349,18 @@ function mouseCurser(){ //myFishSize 0--small, 1---mid, 2----big
 				smallFish[i].style.top=parseInt(smallFish[i].style.top)+t+"px";
 			}
 			else if (smallFish[i].id=="small2"){
-				smallFish[i].style.left=parseInt(smallFish[i].style.left)+l+"px";
-				smallFish[i].style.top=parseInt(smallFish[i].style.top)+t+"px";
+				smallFish[i].style.left=parseInt(smallFish[i].style.left)-l+"px";
+				smallFish[i].style.top=parseInt(smallFish[i].style.top)+t-5+"px";
 			}
 			else if(smallFish[i].id=="small3"){
 				smallFish[i].style.left=parseInt(smallFish[i].style.left)-l+"px";
 				smallFish[i].style.top=parseInt(smallFish[i].style.top)+t-4+"px";
 			}
 
-			if( parseInt(smallFish[i].style.left)>1050 || parseInt(smallFish[i].style.top)>570|| parseInt(smallFish[i].style.left)<200||parseInt(smallFish[i].style.top)<50)
+			if( parseInt(smallFish[i].style.left)>1050 || parseInt(smallFish[i].style.top)==300 || parseInt(smallFish[i].style.left)<200||parseInt(smallFish[i].style.top)<200)
 			{
 				game.removeChild(smallFish[i]);
-				i--;
+				i--;	
 			}
 		}
 	}
@@ -498,9 +385,11 @@ function mouseCurser(){ //myFishSize 0--small, 1---mid, 2----big
 				midFish[i].style.left=parseInt(midFish[i].style.left)-l+"px";
 				midFish[i].style.top=parseInt(midFish[i].style.top)+t-5+"px";
 			}
-			if(parseInt(midFish[i].style.left)>1050||parseInt(midFish[i].style.top)>570 || parseInt(midFish[i].style.left)<200||parseInt(midFish[i].style.top)<50 )
+			if( parseInt(midFish[i].style.left)>1050 || parseInt(midFish[i].style.top)==300 || parseInt(midFish[i].style.left)<200||parseInt(midFish[i].style.top)<200)
 			{
-				game.removeChild(midFish[i]);
+				console.log(smallFish[i].style.left);
+				console.log(smallFish[i].style.top);
+				game.removeChild(smallFish[i]);
 				i--;
 			}
 		}
@@ -523,10 +412,9 @@ function mouseCurser(){ //myFishSize 0--small, 1---mid, 2----big
 				bigFish[i].style.left=parseInt(bigFish[i].style.left)-l+"px";
 				bigFish[i].style.top=parseInt(bigFish[i].style.top)+t+"px";
 			}
-			if(parseInt(bigFish[i].style.left)>1050||parseInt(bigFish[i].style.top)>570 || parseInt(bigFish[i].style.left)<200||parseInt(bigFish[i].style.top)<50)
+			if( parseInt(bigFish[i].style.left)>1050 || parseInt(bigFish[i].style.top)==300 || parseInt(bigFish[i].style.left)<200||parseInt(bigFish[i].style.top)<200)
 			{
 				game.removeChild(bigFish[i]);
-				i--;
 			}
 		}
 	}
@@ -543,39 +431,12 @@ function playNewGame()
 	intiatHeader();
    	setInterval(timer,1000);
    	setInterval(function (){moveSmall();},100);
-	setInterval(function(){creatSmallFish();},1000);
+	setInterval(function(){creatSmallFish();},3000);
 	setInterval(function (){moveMid();},100);
-	setInterval(function(){creatMidFish();},3000);
+	setInterval(function(){creatMidFish();},4000);
 	setInterval(function (){moveBig();},100);
 	setInterval(function(){creatBigFish();},5000);
-
-	drowDiamond();	
-	setInterval(function(){LeftDiamon=true;openLeft();},10000);
-	setInterval(function(){MiddleDiamon=true;openInMiddle();},8000);
-	setInterval(function(){RightDiamon=true;openRight();},12000);
-	setInterval(function(){closeLeft();LeftDiamon=false;},14000);
-	setInterval(function(){closemiddle();MiddleDiamon=false;},12000);
-	setInterval(function(){closeRight();RightDiamon=false;},16000);
-
 	
-}
-function stopPlay()
-{
-	clearInterval(timer,1000);
-   	clearInterval(function (){moveSmall();},100);
-	clearInterval(function(){creatSmallFish();},1000);
-	clearInterval(function (){moveMid();},100);
-	clearInterval(function(){creatMidFish();},3000);
-	clearInterval(function (){moveBig();},100);
-	clearInterval(function(){creatBigFish();},5000);
-	
-	clearInterval(function(){LeftDiamon=true;openLeft();},10000);
-	clearInterval(function(){MiddleDiamon=true;openInMiddle();},8000);
-	clearInterval(function(){RightDiamon=true;openRight();},12000);
-	clearInterval(function(){closeLeft();LeftDiamon=false;},14000);
-	clearInterval(function(){closemiddle();MiddleDiamon=false;},12000);
-	clearInterval(function(){closeRight();RightDiamon=false;},16000);
-
 }
 ///////////////////////////////////////////////////////////////////////
  ///----------update-----------------
@@ -619,6 +480,7 @@ function stopPlay()
 					 //update screen (switch to level 2) with check timer
 					 level=2;
 					 map();
+					 playNewGame();
 					}
 		         break ;
 	      case 2 : 
@@ -628,6 +490,7 @@ function stopPlay()
 					  //update screen (switch to level 3) 
 					  level=3;
 					  map();
+					 playNewGame();
 				 }else {
 					  //time up / /replay level 2 if want 
 					  GameOver(); 
@@ -650,8 +513,7 @@ function stopPlay()
 	
 	function GameOver()
 	{
-		stopPlay();
-		document.getElementsByTagName('game')[0].innerHTML ='<audio id="sound"><source src="sounds/EATGULP.wav" type="audio/mpeg"></source></audio><img src="images/gameover.gif"/>';
+		document.getElementsByTagName('body')[0].innerHTML ='<audio id="sound"><source src="sounds/EATGULP.wav" type="audio/mpeg"></source></audio><img src="images/gameover.gif"/>';
 		if(soundOn){
 			var source = sound.children[0];
 			source.setAttribute('src','sounds/GAMEOVER.wav');
@@ -660,23 +522,8 @@ function stopPlay()
 	}
 	function map()
 	{
-		stopPlay();
-		var menu = document.getElementById("menu");
-		game=document.getElementById("game");
-		document.getElementsByTagName('body')[0].removeChild(game);
-		document.getElementsByTagName('body')[0].style.cursor = "pointer";
-		if(level==2)
-		    document.getElementsByTagName('body')[0].innerHTML +='<div id="game"><header></header><div id="menu" style="margin-bottom:15px"><img src="images/level1.png"/width ="200px" height="200px"><img src="images/level2.png"/width ="200px" height="200px"><img src="images/level3.png"/width ="200px" height="200px"><br/><br/><img src="images/level1.png"/width ="200px" height="200px"><img src="images/done_score.png"/width ="200px" height="100px"><button id="continue"  style=" width:331px; height:68px ; margin-top:60px; margin-left:500px;">Continue</button></div></div>'
-		if(level==3)
-			document.getElementsByTagName('body')[0].innerHTML +='<div id="game"><header></header><div id="menu" style="margin-bottom:15px"><img src="images/level1.png"/width ="200px" height="200px"><img src="images/level2.png"/width ="200px" height="200px"><img src="images/level3.png"/width ="200px" height="200px"><br/><br/><img src="images/level1.png"/width ="200px" height="200px"><img src="images/done_score.png"/width ="200px" height="100px"><button id="continue"  style=" width:331px; height:68px ; margin-top:60px; margin-left:500px;">continue</button></div></div>'
-		var cont = document.getElementById("continue");
-
-	    cont.onclick = function(){
-	   		game=document.getElementById("game");
-   		    menu = document.getElementById("menu"); 
-	  		game.removeChild(menu);
-	  		playNewGame(); 	  			
-		}
+		console.log("map");
+		/////map
 	}
 //////////////////////////////
 window.onload=function(){
